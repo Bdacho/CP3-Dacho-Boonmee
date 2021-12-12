42663c13eabb56c0f29afcfaefcffe38105d1da8
Bdacho <bdacho.j@gmail.com>
2021-12-12 19:34:31 +0700

    Create Exercise5_2_Dacho_B.py
    การคำนวณหาระยะทางที่เคลื่อนที่

---
+++ Exercise5_2_Dacho_B.py
@@ -0,0 +1,10 @@
+'''
+Exercise5_2
+ตัวอย่างรูปแบบ Output ที่แสดงผลโปรแกรม
+- กรณีตัวแปรระยะทางคือ 120 และ ตัวแปรเวลาคือ 10
+'''
+
+s=int(input("Distance is ="))
+t=int(input("Time is ="))
+v=int(s/t)
+print(v,"km/h")
