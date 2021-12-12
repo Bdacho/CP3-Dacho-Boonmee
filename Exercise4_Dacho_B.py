593067c348a4095662c3a86073f94a35d517d8a6
Bdacho <bdacho.j@gmail.com>
2021-12-12 17:49:49 +0700

    Create Exercise4_Dacho_B.py
    Exercise 4

---
+++ Exercise4_Dacho_B.py
@@ -0,0 +1,11 @@
+aa=97.5
+ab=99.8
+ac=89.1
+ad=69.99
+
+print("--- Your Score ---")
+print("Foundation English               : ",aa)
+print("General Business                 : ",ab)
+print("Introduction to Computer Systems : ",ac)
+
+print("Computer Programming             : ",ad)
