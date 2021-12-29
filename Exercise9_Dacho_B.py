4b0af53bfbbeb3700f69175458eed0d80611fe85
Bdacho <bdacho.j@gmail.com>
2021-12-29 17:42:09 +0700

    Exercise9
    WhileExample

---
+++ Exercise9_Dacho_B.py
@@ -0,0 +1,9 @@
+usernameInput=input("Username : ")
+passwordInput=input("Password : ")
+
+while usernameInput!="admin"or passwordInput!="123qwe":
+    print("Please try again!!")
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+
+print("Done")
